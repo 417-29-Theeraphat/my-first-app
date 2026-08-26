acd47ad0669998c5c530e0152cd757e51f7603cb
417-29-Theeraphat <theepungpond@gmail.com>
2026-08-26 11:40:08 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-net_price = price - vat
 vat = price * 0.07
+net_price = price - vat
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
 st.write("นาย ธีรภัทร ธรรมใจ เลขที่ 29  ม.4/17")
